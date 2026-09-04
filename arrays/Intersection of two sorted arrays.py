a4a6fd6f1765b57b4ee02589fe70f8083bdbbe07
samvrutha-sekar <samvruthasekar@gmail.com>
2026-09-04 22:51:33 +0530

    Add intersection of two sorted arrays solution

---
+++ arrays/Intersection of two sorted arrays.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intersectionArray(self, nums1, nums2):
+          i = 0
+    j = 0
+    result = []
+
+    while i < len(nums1) and j < len(nums2):
+        if nums1[i] == nums2[j]:
+            result.append(nums1[i])
+            i += 1
+            j += 1
+        elif nums1[i] < nums2[j]:
+            i += 1
+        else:
+            j += 1
+
+    return result  
